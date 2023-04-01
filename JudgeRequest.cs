515437a3ed88530fe3c6c0c10c09722b52440e05
hayoonleeMe <htdobad@gmail.com>
2023-04-01 17:49:45 +0900

    채점 요청 모델 클래스 생성

---
+++ JudgeRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace JudgeServer {
+    public class JudgeRequest {
+        // 문제를 식별할 수 있는 Id
+        public string? Id { get; set; }
+        // 에디터에 작성하여 제출한 코드
+        public string? Code { get; set; }
+    }
+}
