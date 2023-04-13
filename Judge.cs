a7e810a8cada822c85ee4460ff178840183ed92a
LeeHaYoon <htdobad@gmail.com>
2023-04-13 16:25:17 +0900

    채점 제출 폴더를 삭제하는 코드 활성화

--- Judge.cs
+++ Judge.cs
@@ -129,8 +129,7 @@ namespace JudgeServer {
             }
 
             // 채점 제출 폴더 삭제
-            // TODO : 현재는 디버깅을 위해 주석처리
-            //DeleteSubmitFolder(in folderPath);
+            DeleteSubmitFolder(in folderPath);
 
             // 모든 테스트 케이스를 수행하면 결과를 저장해 JudgeResult 객체 반환
             return GetJudgeResult(in caseCount, ref result, ref avgExecutionTime, ref avgMemoryUsage);
