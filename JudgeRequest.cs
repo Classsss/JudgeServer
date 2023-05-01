ebee6bbe44317743ac9c42cf88583ba712536a7f
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2023-05-01 18:12:46 +0900

    Code 제출과 교수 두개로 수정

--- JudgeRequest.cs
+++ JudgeRequest.cs
@@ -1,6 +1,8 @@
 ﻿namespace JudgeServer {
     public class JudgeRequest {
-        public string Code { get; set; }                  // 에디터에 작성하여 제출한 코드
+
+        public string CorrectCode { get; set; }           // 교수가 제출한 정답 코드
+        public string SubmitCode { get; set; }            // 학생이 에디터에 작성하여 제출한 코드
         public string Language { get; set; }              // 코드의 프로그래밍 언어
         public List<string> InputCases { get; set; }      // 입력 테스트 케이스
         public double ExecutionTimeLimit { get; set; }    // 실행 시간 제한
